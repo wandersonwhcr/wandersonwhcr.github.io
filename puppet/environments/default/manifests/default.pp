3188f498380dc6f4d436e6db770a680ac7378ddc
Wanderson <wandersonwhcr@gmail.com>
2017-01-19 20:32:55 -0200

    Configura a atualização de pacotes do Debian

---
+++ puppet/environments/default/manifests/default.pp
@@ -0,0 +1,23 @@
+Exec {
+    path    => ["/usr/bin", "/bin", "/usr/sbin", "/sbin" ],
+    timeout => 0,
+}
+
+Package {
+    require => Exec["apt-get : update"],
+    before  => Exec["apt-get : autoremove"],
+}
+
+exec { "apt-get : update":
+    command => "apt-get update",
+}
+
+exec { "apt-get : upgrade":
+    command => "apt-get upgrade -q -y",
+    require => Exec["apt-get : update"],
+}
+
+exec { "apt-get : autoremove":
+    command => "apt-get autoremove -q -y",
+    require => Exec["apt-get : upgrade"],
+}
