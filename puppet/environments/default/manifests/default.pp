f8ebbfe11ac6d07f7ff5132539503613c3084d07
Wanderson <wandersonwhcr@gmail.com>
2017-01-20 12:39:43 -0200

    Remove instalação do PHP por Puppet

--- puppet/environments/default/manifests/default.pp
+++ puppet/environments/default/manifests/default.pp
@@ -48,37 +48,3 @@ package { "rpcbind":
     name   => "rpcbind",
     ensure => "purged",
 }
-
-exec { "php : key":
-    unless  => "apt-key list | grep dotdeb",
-    command => "curl http://www.dotdeb.org/dotdeb.gpg | apt-key add -",
-}
-
-file { "php : list":
-    path    => "/etc/apt/sources.list.d/php.list",
-    content => "deb http://packages.dotdeb.org jessie all",
-    require => Exec["php : key"],
-    before  => Exec["apt-get : update"],
-}
-
-package { "php : cli":
-    name    => "php-cli",
-    require => [
-        File["php : list"],
-        Exec["apt-get : update"],
-    ],
-}
-
-exec { "composer":
-    creates => "/usr/bin/composer",
-    command => "curl https://getcomposer.org/composer.phar -o /usr/bin/composer && chmod +x /usr/bin/composer",
-}
-
-exec { "composer : update":
-    command     => "composer self-update",
-    environment => "COMPOSER_HOME=/root/.composer",
-    require     => [
-        Package["php : cli"],
-        Exec["composer"],
-    ],
-}
